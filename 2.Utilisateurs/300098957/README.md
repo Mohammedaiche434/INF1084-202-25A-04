3fa473775772a621fa3c7270ef62ed305c22ab7f
brobert <brobert@valiha.com>
2025-09-30 17:29:45 -0400

    :ab: Mon premier script PWSH

---
+++ 2.Utilisateurs/300098957/README.md
@@ -0,0 +1,9 @@
+# 300098957
+
+:star: C'est moi
+
+## Exercice :one:
+
+```powershell
+.\utilisateurs1.ps1
+```
---
+++ 2.Utilisateurs/300098957/utilisateurs1.ps1
@@ -0,0 +1,10 @@
+# Créer une liste d'utilisateurs simulés
+$Users = @(
+    @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
+    @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
+    @{Nom="Ngue"; Prenom="Taylor"; Login="tngue"; OU="Etudiants"}
+)
+
+# Afficher les utilisateurs
+$Users | ForEach-Object { "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)" }
