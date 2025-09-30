efb6f082ca6f830c9b7bc1e8f151f23bb2ba9cac
lounasallouti1 <sahit.sahitou@gmail.com>
2025-09-30 23:02:11 +0100

    Organisation des scripts utilisateurs dans le dossier 300150295

---
+++ 2.Utilisateurs/300150295/utilisateurs3.ps1
@@ -0,0 +1,8 @@
+# utilisateurs3.ps1
+System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable = @(
+    @{Nom="Gauthier"; Prenom="Nina"; Login="ngauthier"; OU="Stagiaires"},
+    @{Nom="Rousseau"; Prenom="Thomas"; Login="trousseau"; OU="Stagiaires"}
+)
+System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable += @{Nom="Marchand"; Prenom="Léa"; Login="lmarchand"; OU="Stagiaires"}
+System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable += @{Nom="Blanc"; Prenom="Victor"; Login="vblanc"; OU="Stagiaires"}
+System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable System.Collections.Hashtable | ForEach-Object { "  - Login:  - OU: " }
